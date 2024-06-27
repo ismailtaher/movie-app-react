6512fa1e941c1a594b0dc040a0c4ab8749636ad1
ismailtaher <ismail.taher@ymail.com>
2024-06-27 19:28:47 +0500

    13th Commit

--- src/HomePosts.js
+++ src/HomePosts.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const HomePosts = ({ movie, genres }) => {
+const HomePosts = ({ movie }) => {
   const imgUrl = "https://image.tmdb.org/t/p/w500";
 
-  const movieGenresIds = new Set(movie.genre_ids);
+  /* const movieGenresIds = new Set(movie.genre_ids);
 
   const commonGenres = genres
     .filter((genre) => movieGenresIds.has(genre.id))
     .map((genre) => genre.name)
-    .join(" | ");
+    .join(" | "); */
 
   return (
     <article className="flex flex-wrap md:w-[100%]">
